0c1bafacfeb5947925d34d03224bd571ffa0e065
cchong0421 <CCWeb0pass>
2015-12-01 11:23:24 +0800

    First Version

---
+++ QRCodeGenerator/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace QRCodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtVCardResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtORG = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFN = new System.Windows.Forms.TextBox();
+            this.txtDepart = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtWork = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtHome = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(306, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(247, 226);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Create QRCode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtVCardResult
+            // 
+            this.txtVCardResult.Font = new System.Drawing.Font("新細明體", 11F);
+            this.txtVCardResult.Location = new System.Drawing.Point(171, 255);
+            this.txtVCardResult.Multiline = true;
+            this.txtVCardResult.Name = "txtVCardResult";
+            this.txtVCardResult.ReadOnly = true;
+            this.txtVCardResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtVCardResult.Size = new System.Drawing.Size(382, 116);
+            this.txtVCardResult.TabIndex = 2;
+            this.txtVCardResult.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(17, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "姓氏 / 名字";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(85, 20);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 22);
+            this.txtLastName.TabIndex = 0;
+            this.txtLastName.Leave += new System.EventHandler(this.txtLastName_Leave);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(191, 20);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 22);
+            this.txtFirstName.TabIndex = 1;
+            this.txtFirstName.Leave += new System.EventHandler(this.txtFirstName_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(17, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "公司";
+            // 
+            // txtORG
+            // 
+            this.txtORG.Location = new System.Drawing.Point(85, 48);
+            this.txtORG.Name = "txtORG";
+            this.txtORG.Size = new System.Drawing.Size(206, 22);
+            this.txtORG.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(17, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "歸檔為";
+            // 
+            // txtFN
+            // 
+            this.txtFN.Location = new System.Drawing.Point(85, 104);
+            this.txtFN.Name = "txtFN";
+            this.txtFN.ReadOnly = true;
+            this.txtFN.Size = new System.Drawing.Size(206, 22);
+            this.txtFN.TabIndex = 4;
+            this.txtFN.TabStop = false;
+            // 
+            // txtDepart
+            // 
+            this.txtDepart.Location = new System.Drawing.Point(85, 76);
+            this.txtDepart.Name = "txtDepart";
+            this.txtDepart.Size = new System.Drawing.Size(206, 22);
+            this.txtDepart.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label4.Location = new System.Drawing.Point(17, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "部門";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(85, 132);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(206, 22);
+            this.txtMobile.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label5.Location = new System.Drawing.Point(17, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "手機";
+            // 
+            // txtWork
+            // 
+            this.txtWork.Location = new System.Drawing.Point(85, 160);
+            this.txtWork.Name = "txtWork";
+            this.txtWork.Size = new System.Drawing.Size(206, 22);
+            this.txtWork.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label6.Location = new System.Drawing.Point(17, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "公司電話";
+            // 
+            // txtHome
+            // 
+            this.txtHome.Location = new System.Drawing.Point(85, 188);
+            this.txtHome.Name = "txtHome";
+            this.txtHome.Size = new System.Drawing.Size(206, 22);
+            this.txtHome.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label7.Location = new System.Drawing.Point(17, 188);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "家中電話";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(85, 216);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(206, 22);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label8.Location = new System.Drawing.Point(17, 216);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "電子郵件";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 39);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Save Contact vCard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 389);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtHome);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtWork);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDepart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtORG);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVCardResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "產生名片QR Code";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtVCardResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtORG;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFN;
+        private System.Windows.Forms.TextBox txtDepart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtWork;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtHome;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ QRCodeGenerator/Form1.cs
@@ -0,0 +1,116 @@
+﻿/* =========================================================================================
+ * === 程式說明
+ * === 專案名稱：QRCodeGenerator (QRCode 名片產生器)
+ * =========================================================================================
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace QRCodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtLastName.Text))
+            {
+                MessageBox.Show("姓氏不可為空白");
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("BEGIN:VCARD\r\n");
+            sb.Append("VERSION:2.1\r\n");
+            sb.AppendFormat("N;LANGUAGE=zh-tw;CHARSET=utf-8:{0};{1}\r\n", txtLastName.Text.Trim(), txtFirstName.Text.Trim());
+
+            if (string.IsNullOrEmpty(txtFN.Text))
+                txtFN.Text = string.Format("{0}{1}", txtLastName.Text, txtFirstName.Text);
+            sb.AppendFormat("FN;CHARSET=utf-8:{0}\r\n", txtFN.Text.Trim());
+
+            if (!string.IsNullOrEmpty(txtORG.Text))
+                sb.AppendFormat("ORG;CHARSET=utf-8:{0};{1}\r\n", txtORG.Text.Trim(), txtDepart.Text.Trim());
+
+            if (!string.IsNullOrEmpty(txtMobile.Text))
+                sb.AppendFormat("TEL;CELL;VOICE:{0}\r\n", txtMobile.Text.Trim());
+
+            if (!string.IsNullOrEmpty(txtWork.Text))
+                sb.AppendFormat("TEL;WORK;VOICE:{0}\r\n", txtWork.Text.Trim());
+
+            if (!string.IsNullOrEmpty(txtHome.Text))
+                sb.AppendFormat("TEL;HOME;VOICE:{0}\r\n", txtHome.Text.Trim());
+
+            if (!string.IsNullOrEmpty(txtEmail.Text))
+                sb.AppendFormat("EMAIL;PREF;INTERNET:{0}\r\n", txtEmail.Text.Trim());
+
+            sb.AppendFormat("REV:{0}\r\n", DateTime.Now.ToString("yyyyMMddThhmmssZ"));
+            sb.Append("END:VCARD");
+
+            txtVCardResult.Text = sb.ToString();
+
+            string sEncode = "utf-8";
+            string EncoderContent = GetEncodingString(txtVCardResult.Text, sEncode);
+
+            BarcodeWriter bw = new BarcodeWriter();
+            bw.Format = BarcodeFormat.QR_CODE;
+            bw.Options.Width = 260;
+            bw.Options.Height = 237;
+            bw.Options.PureBarcode = true;
+
+            Bitmap bitmap = bw.Write(EncoderContent);
+            pictureBox1.Image = (Image)bitmap;
+        }
+
+        private void txtLastName_Leave(object sender, EventArgs e)
+        {
+            txtFN.Text = string.Format("{0}{1}", txtLastName.Text, txtFirstName.Text);
+        }
+        private void txtFirstName_Leave(object sender, EventArgs e)
+        {
+            txtFN.Text = string.Format("{0}{1}", txtLastName.Text, txtFirstName.Text);
+        }
+        private string GetEncodingString(string srcString, string sEncode)
+        {
+            Encoding e8859Encode = Encoding.GetEncoding("iso-8859-1");
+            Encoding srcEncode = Encoding.Unicode;
+            Encoding dstEncode = Encoding.GetEncoding(sEncode);
+            byte[] srcBytes = srcEncode.GetBytes(srcString);
+            byte[] dstBytes = Encoding.Convert(srcEncode, dstEncode, srcBytes);
+            char[] dstChars = new char[e8859Encode.GetCharCount(dstBytes, 0, dstBytes.Length)];
+            e8859Encode.GetChars(dstBytes, 0, dstBytes.Length, dstChars, 0);
+            return new string(dstChars);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Png Image|*.png";
+            dialog.Title = "Save an Image File";
+            dialog.InitialDirectory = "C:\\";
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (dialog.FileName != "")
+                {
+                    pictureBox1.Image.Save(dialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                    MessageBox.Show("Save done!!");
+                }
+            }
+            
+        }
+    }
+}
